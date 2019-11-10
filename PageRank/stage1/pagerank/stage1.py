efe942edcc61bcf7f3fcc1b9caecdeb9ad0b1c3a
gelkin <mazin.v.a@gmail.com>
2019-11-10 17:48:58 +0100

    added testing library and printing with print

--- PageRank/stage1/pagerank/stage1.py
+++ PageRank/stage1/pagerank/stage1.py
@@ -1,31 +1,31 @@
+from io import StringIO
+
 import numpy as np
 import numpy.linalg as la
-
-L = np.array([[0,   1/2, 1/3, 0, 0,   0 ],
-              [1/3, 0,   0,   0, 1/2, 0 ],
-              [1/3, 1/2, 0,   1, 0,   1/2 ],
-              [1/3, 0,   1/3, 0, 1/2, 1/2 ],
-              [0,   0,   0,   0, 0,   0 ],
-              [0,   0,   1/3, 0, 0,   0 ]])
-
-#print(L)
 import sys
+
+# if __name__ == "__main__":
+L = np.array([[0, 1 / 2, 1 / 3, 0, 0, 0],
+              [1 / 3, 0, 0, 0, 1 / 2, 0],
+              [1 / 3, 1 / 2, 0, 1, 0, 1 / 2],
+              [1 / 3, 0, 1 / 3, 0, 1 / 2, 1 / 2],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 1 / 3, 0, 0, 0]])
+
+
 np.savetxt(sys.stdout, L, fmt="%.3f")
 
-# find eigenvalues and eigenvectors
-eVals, eVecs = la.eig(L)  # Gets the eigenvalues and vectors
-# print a real part of eigenvalues
-# print(np.real(eVals))
+eVals, eVecs = la.eig(L)
 
-order = np.absolute(eVals).argsort()[::-1]  # Orders them by their eigenvalues
+order = np.absolute(eVals).argsort()[::-1]
 eVals = eVals[order]
 eVecs = eVecs[:, order]
 
+r = eVecs[:, 0]
 
-r = eVecs[:, 0]  # Sets r to be the principal eigenvector
-# print(r)
-# np.real(r)
-
-# print(100 * np.real(r / np.sum(r)))
 r = 100 * np.real(r / np.sum(r))
-np.savetxt(sys.stdout, r, fmt="%.3f")
+s = StringIO()
+np.savetxt(s, r, fmt="%.3f")
+print(s.getvalue())
+
+# todo fix the documents sys -> StringIO
